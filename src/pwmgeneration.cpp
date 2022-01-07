5bcf88367c7b5956e57d0042c64559d4bd1d3e35
kon <k@smo.la>
2022-01-06 20:20:45 -0800

    prep for merge from upstream

--- src/pwmgeneration.cpp
+++ src/pwmgeneration.cpp
@@ -16,8 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <libopencm3/stm32/timer.h>
-#include <libopencm3/stm32/rcc.h>
+// #include <libopencm3/stm32/timer.h>
+// #include <libopencm3/stm32/rcc.h>
 #include "pwmgeneration.h"
 #include "hwdefs.h"
 #include "params.h"
